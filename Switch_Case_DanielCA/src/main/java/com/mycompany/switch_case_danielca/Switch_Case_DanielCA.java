7707dcb1121532b2d37c36c8f0ae1afade45f438
Daniel Cristancho <dasancristal91@gmail.com>
2025-05-18 07:01:17 -0500

    Update Switch_Case_DanielCA.java

--- Switch_Case_DanielCA/src/main/java/com/mycompany/switch_case_danielca/Switch_Case_DanielCA.java
+++ Switch_Case_DanielCA/src/main/java/com/mycompany/switch_case_danielca/Switch_Case_DanielCA.java
@@ -1,17 +1,30 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package SwitchCase_Daniel;
 
-package com.mycompany.switch_case_danielca;
+import java.util.Scanner;
 
-/**
- *
- * @author Estudiantes
- */
-public class Switch_Case_DanielCA {
+public class SwitchCase_Daniel {
+
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner Scanner = new Scanner(System.in);
+        
+      System.out.println("ingrese el caso a escanear: ");
+      int dia = Scanner.nextInt();
+        
+        switch (dia) {
+            case 1 -> System.out.println("lunes");
+            case 2 -> System.out.println("martes");
+            case 3 -> System.out.println("miercoles");
+            case 4 -> System.out.println("jueves");
+            case 5 -> System.out.println("viernes");
+            case 6 -> System.out.println("sabado");
+            case 7 -> System.out.println("domingo");
+            default -> System.out.println("Número de día inválido");
+        }            
     }
+    
 }
