7380de8aa2b58eed7b1160894bbfbea8142d422a
Daniel Cristancho <dasancristal91@gmail.com>
2025-05-18 19:55:40 -0500

    Update Switch_Case_DanielCA.java

--- Switch_Case_DanielCA/src/main/java/com/mycompany/switch_case_danielca/Switch_Case_DanielCA.java
+++ Switch_Case_DanielCA/src/main/java/com/mycompany/switch_case_danielca/Switch_Case_DanielCA.java
@@ -14,12 +14,12 @@ public class SwitchCase_Daniel {
 
 switch(dia) {
         case 1 -> System.out.println("Lunes");
-        case 2 -> System.out.println("Lunes");
-        case 3 -> System.out.println("Lunes");
-        case 4 -> System.out.println("Lunes");
-        case 5 -> System.out.println("Lunes");
-        case 6 -> System.out.println("Lunes");
-        case 7 -> System.out.println("Lunes");
+        case 2 -> System.out.println("Martes");
+        case 3 -> System.out.println("Miercoles");
+        case 4 -> System.out.println("Jueves");
+        case 5 -> System.out.println("Viernes");
+        case 6 -> System.out.println("Sabado");
+        case 7 -> System.out.println("Domingo");
         default -> System.out.println("Numéro no válido!")
     }
 }
